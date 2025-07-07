69dd10b3fe3cb1c6e5e42ffa725769e62e4f35f8
Morn Soda <mornsoda23@gmail.com>
2025-07-07 13:09:42 +0700

    added

--- Frontend/src/plugins/axios.js
+++ Frontend/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
   withCredentials: true, // Important if using Sanctum
   headers: {
     "Content-Type": "application/json",
