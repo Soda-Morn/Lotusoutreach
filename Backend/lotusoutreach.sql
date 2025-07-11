2d87320ca885db5ee0d9dde64482f44c058b7a45
Morn Soda <mornsoda23@gmail.com>
2025-07-11 11:04:00 +0700

    add

---
+++ Backend/lotusoutreach.sql
@@ -0,0 +1,548 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2025 at 01:56 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lotusoutreach`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approaches`
+--
+
+CREATE TABLE `approaches` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image_src` varchar(255) NOT NULL,
+  `image_alt` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approach_tabs`
+--
+
+CREATE TABLE `approach_tabs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `approach_id` bigint(20) UNSIGNED NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cta_sections`
+--
+
+CREATE TABLE `cta_sections` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `quote` text NOT NULL,
+  `button_text` varchar(255) NOT NULL,
+  `image_src` varchar(255) NOT NULL,
+  `image_alt` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `focus_areas`
+--
+
+CREATE TABLE `focus_areas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `focus_cards`
+--
+
+CREATE TABLE `focus_cards` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `focus_area_id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `summary` text DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hero_images`
+--
+
+CREATE TABLE `hero_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `page_content_id` bigint(20) UNSIGNED NOT NULL,
+  `src` varchar(255) NOT NULL,
+  `alt` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `hero_images`
+--
+
+INSERT INTO `hero_images` (`id`, `page_content_id`, `src`, `alt`, `created_at`, `updated_at`) VALUES
+(4, 2, 'hero_image/pZp7qZzJwYe22uTUsUgRCN33z78RGaBa7IMqNEZx.jpg', 'Cambodian Children and Families', '2025-07-10 11:19:50', '2025-07-10 11:19:50'),
+(5, 2, 'hero_image/2MbrsGjg7MgQIl8J5aGZHqOgnrqHt3iWKp1tJQBr.jpg', 'Cambodian Children and Families', '2025-07-10 11:23:29', '2025-07-10 11:23:29'),
+(6, 2, 'hero_image/9hG7TxgN4Jju5q7lJjbFQdf14Po9XvDZahrmnm3T.jpg', 'Cambodian Children and Families', '2025-07-10 11:23:39', '2025-07-10 11:23:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `impact_states`
+--
+
+CREATE TABLE `impact_states` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `target` int(11) NOT NULL,
+  `suffix` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2025_07_10_025833_create_page_contents_table', 1),
+(6, '2025_07_10_051146_create_hero_images_table', 1),
+(7, '2025_07_10_051829_create_impact_states_table', 1),
+(8, '2025_07_10_051903_create_missions_table', 1),
+(9, '2025_07_10_051927_create_mission_cards_table', 1),
+(10, '2025_07_10_052006_create_focus_areas_table', 1),
+(11, '2025_07_10_052023_create_focus_cards_table', 1),
+(12, '2025_07_10_052038_create_approaches_table', 1),
+(13, '2025_07_10_052055_create_approach_tabs_table', 1),
+(14, '2025_07_10_052114_create_water_initiatives_table', 1),
+(15, '2025_07_10_052144_create_cta_sections_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `missions`
+--
+
+CREATE TABLE `missions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mission_cards`
+--
+
+CREATE TABLE `mission_cards` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `mission_id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `title_class` varchar(255) DEFAULT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
+  `alt` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_contents`
+--
+
+CREATE TABLE `page_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `page_contents`
+--
+
+INSERT INTO `page_contents` (`id`, `title`, `subtitle`, `description`, `created_at`, `updated_at`) VALUES
+(2, 'Empowering Cambodia\'s Girls', 'Cambodia', 'n rural Cambodia, education transforms lives. When you empower a Cambodian girl, you transform an entire community.', '2025-07-10 09:13:10', '2025-07-10 09:13:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `water_initiatives`
+--
+
+CREATE TABLE `water_initiatives` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `highlight` varchar(255) DEFAULT NULL,
+  `stat_value` int(11) DEFAULT NULL,
+  `stat_text` varchar(255) DEFAULT NULL,
+  `image_src` varchar(255) DEFAULT NULL,
+  `image_alt` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `approaches`
+--
+ALTER TABLE `approaches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `approach_tabs`
+--
+ALTER TABLE `approach_tabs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `approach_tabs_approach_id_foreign` (`approach_id`);
+
+--
+-- Indexes for table `cta_sections`
+--
+ALTER TABLE `cta_sections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `focus_areas`
+--
+ALTER TABLE `focus_areas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `focus_cards`
+--
+ALTER TABLE `focus_cards`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `focus_cards_focus_area_id_foreign` (`focus_area_id`);
+
+--
+-- Indexes for table `hero_images`
+--
+ALTER TABLE `hero_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hero_images_page_content_id_foreign` (`page_content_id`);
+
+--
+-- Indexes for table `impact_states`
+--
+ALTER TABLE `impact_states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `missions`
+--
+ALTER TABLE `missions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mission_cards`
+--
+ALTER TABLE `mission_cards`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `mission_cards_mission_id_foreign` (`mission_id`);
+
+--
+-- Indexes for table `page_contents`
+--
+ALTER TABLE `page_contents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `water_initiatives`
+--
+ALTER TABLE `water_initiatives`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `approaches`
+--
+ALTER TABLE `approaches`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `approach_tabs`
+--
+ALTER TABLE `approach_tabs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cta_sections`
+--
+ALTER TABLE `cta_sections`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `focus_areas`
+--
+ALTER TABLE `focus_areas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `focus_cards`
+--
+ALTER TABLE `focus_cards`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hero_images`
+--
+ALTER TABLE `hero_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `impact_states`
+--
+ALTER TABLE `impact_states`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `missions`
+--
+ALTER TABLE `missions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mission_cards`
+--
+ALTER TABLE `mission_cards`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page_contents`
+--
+ALTER TABLE `page_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `water_initiatives`
+--
+ALTER TABLE `water_initiatives`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `approach_tabs`
+--
+ALTER TABLE `approach_tabs`
+  ADD CONSTRAINT `approach_tabs_approach_id_foreign` FOREIGN KEY (`approach_id`) REFERENCES `approaches` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `focus_cards`
+--
+ALTER TABLE `focus_cards`
+  ADD CONSTRAINT `focus_cards_focus_area_id_foreign` FOREIGN KEY (`focus_area_id`) REFERENCES `focus_areas` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `hero_images`
+--
+ALTER TABLE `hero_images`
+  ADD CONSTRAINT `hero_images_page_content_id_foreign` FOREIGN KEY (`page_content_id`) REFERENCES `page_contents` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `mission_cards`
+--
+ALTER TABLE `mission_cards`
+  ADD CONSTRAINT `mission_cards_mission_id_foreign` FOREIGN KEY (`mission_id`) REFERENCES `missions` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
